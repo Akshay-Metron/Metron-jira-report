2972bdf8b82a63b2bd5a9c05bc5aa4418ea2b82b
Akshay-metron <akshay@metronlab.com>
2022-02-23 19:03:21 +0530

    Implemented multi lines comment with bullet point and submit button

--- src/index.jsx
+++ src/index.jsx
@@ -1,144 +1,145 @@
-import ForgeUI, { render, ProjectPage, Fragment, Text, Table, Head, Row, Cell, useProductContext, useState, Heading, DatePicker, Form,useEffect } from '@forge/ui';
+import ForgeUI,
+{
+  render,
+  ProjectPage,
+  Fragment,
+  Text,
+  Table,
+  Head,
+  Row,
+  Cell,
+  useProductContext,
+  useState,
+  Heading,
+  DatePicker,
+  Form,
+  useEffect
+}
+  from '@forge/ui';
+
 import api, { route } from "@forge/api";
 
+import moment from 'moment';
 
 
-var issue_keys=[]    
-var fetch_worklog=[];
+
+
+var issue_keys = []
+var fetch_worklog = [];
 var all_WorkLogs = []
-
+var projectkey = "";
 const fetchworklogsForIssue = async (issueId) => {
- 
+
   for (let i = 0; i < issue_keys.length; i++) {
     const element = issue_keys[i];
-    //for (let j = 0; j < issue_keys.length; j++) {
-      //const element = issue_keys[i];
-      
-    
-    
-    const response = await api.asApp().requestJira(route`/rest/api/3/issue/${element}/worklog`, {
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
+
+
+
+
+    const response = await api.asApp().requestJira(route`/rest/api/3/issue/${element}/worklog`,
+      {
+        headers:
+        {
+          'Accept': 'application/json'
+        }
+
+      });
+
+
     fetch_worklog = await response.json();
-    all_WorkLogs.push(fetch_worklog.worklogs);
-    
-    
-    
-  //}
+    all_WorkLogs.push(...fetch_worklog.worklogs);
+
+
+  }
+  //console.log(all_WorkLogs[3].comment.content[0].content[1].content[0]);
+
+  //console.log(all_WorkLogs[3].comment.content[0].content[1].content[0].content[0].text);
+  return fetch_worklog
 }
-//console.log(all_WorkLogs[1]);
-  return fetch_worklog;
-};
 
 
-/*
-const fetchworklogsByUsingIssue = async (issueId) => {
-  console.log("Hello");
-  const response = await api.asApp().requestJira(route`/rest/api/3/issue/${issueId}/worklog`, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-  console.log("output ============ >"+`Response: ${response.status} ${response.statusText}`);
-   
-  const fetch_issueid = await response.json();
-  console.log(data);
 
-  
-  return fetch_issueid;
-};*/
 
 
 
 const projectStatuses = async (issueId) => {
-  const response4 = await api.asApp().requestJira(route`/rest/api/3/search?jql=project%20%3D%20ITSAMPLE`, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
+  const response4 = await api.asApp().requestJira(route`/rest/api/3/search?jql=project%20%3D%20${projectkey}`,
+    {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
 
   const fetch_project = await response4.json();
   //console.log(`Response4: ${response4.status} ${response4.statusText}`);
   //console.log(await response4.json());
-  //console.log(fetch_project.issues[0].fields);
+
   return fetch_project
 };
 
+ var date = new Date();
+ var firstDay = moment(new Date(date.getFullYear(), date.getMonth(), 1)).format('YYYY-MM-DD');
+ var today= moment(new Date().toLocaleString() + "").format('YYYY-MM-DD')
+ //var lastDay = moment(new Date(date.getFullYear(), date.getMonth() + 1, 0)).format('YYYY-MM-DD');
 
-
-const fetchprojectdetail = async (issueId) => {
-  const response2 = await api.asApp().requestJira(route`/rest/api/3/project`, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-
-  //console.log("==============================new=============");
-  //console.log(`Response2: ${response2.status} ${response2.statusText}`);
-  //console.log(await response2.json());
-  const fetch_project_details = await response2.json();
-  //console.log(fetch_project_details);
-  return fetch_project_details;
-};
-
-
-
-
-
-const fetchsummary = async (issueId) => {
-  const response3 = await api.asApp().requestJira(route`/rest/api/3/issue/ITSAMPLE-6?fields=summary,description;`, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-
-  //console.log("==============================new=============");
-  //console.log(`Response3: ${response3.status} ${response3.statusText}`);
-  //console.log(await response3.json());
-  const fetch_summary = await response3.json();
-  //console.log(fetch_summary);
-  return fetch_summary;
-};
-//=============================xxxxxxxxxxxxxxxxxxxxxxxxxxxx===========================================//
-
-
+ console.log(firstDay);
+ //console.log(lastDay);
+ console.log(today);
+var startDate = firstDay
+var endDate = today
+//const [all_worklog]= useState(async () => await fetchworklogsForIssue(context.platformContext.issueKey));
 const App = () => {
 
+  const [formState, setFormState] = useState(undefined)
   const context = useProductContext();
- // const [fetch_worklog] = useState(async () => await fetchworklogsForIssue(context.platformContext.issueKey));
-  const [fetch_project_details] = useState(async () => await fetchprojectdetail(context.platformContext.issueKey));
-  const [fetch_summary] = useState(async () => await fetchsummary(context.platformContext.issueKey));
-  const [fetch_project] = useState(async () => await projectStatuses(context.platformContext.issueKey));
-  
 
-  var totalHours=0
+  const onSubmit = async (formData) => {
+
+    formData:
+    {
+
+        startDate: 's_date';
+        endDate: 'e_date';
+      
+    }
+    console.log(formData);
+    setFormState(formData)
+    startDate = formData.s_date
+    endDate=formData.e_date
+
+    const all_WorkLogs = await fetchworklogsForIssue(context.platformContext.issueKey);
+    
+  }
+
+  projectkey = context["platformContext"]["projectKey"]
+  //const [fetch_worklog] = useState(async () => await fetchworklogsForIssue(context.platformContext.issueKey));
+  const [fetch_project] = useState(async () => await projectStatuses(context.platformContext.issueKey));
+
+  
+  var totalHours = 0
+
   const issueKeys = fetch_project.issues;
+
 
   for (let i = 0; i < issueKeys.length; i++) {
     const element = issueKeys[i];
-    //console.log("==================================for loop===============================================");
-    //console.log(element.key);
-    
-    totalHours=totalHours+(element.fields.timespent)/3600;
-   // const [fetch_worklog] = useState(async () => await fetchworklogsForIssue(element.key));
-   
-    //console.log(fetch_worklog.worklogs[0].comment.content[0].content[0].text);
-    //console.log(fetch_worklog.worklogs[0].started);
 
-    issue_keys[i]=element.key
+    if ((moment(element.fields.created).format('YYYY-MM-DD'))>=startDate  && (moment(element.fields.created).format('YYYY-MM-DD')<=endDate))
+    totalHours = totalHours + (element.fields.timespent) / 3600;
+    //console.log(moment(element.fields.created).format('YYYY-MM-DD'));
+
+    issue_keys[i] = element.key
   }
 
-  const [fetch_worklog] = useState(async () => await fetchworklogsForIssue(context.platformContext.issueKey));
-  console.log("==================================");
-  console.log(all_WorkLogs[0][0].started);
-  console.log("====================================");
+  
+
+
 
   return (
     <Fragment>
+
       <Heading size="large">Metron Labs support hours report </Heading>
-      
+
 
       <Table>
 
@@ -146,50 +147,48 @@ const App = () => {
 
         <Row>
           <Cell>
-            <Text>Project Name</Text>
-          </Cell>
-          <Cell>
-            <Text>{fetch_project.issues[0].fields.project.name}</Text>
+            <Heading size='medium'>{fetch_project.issues[0].fields.project.name}</Heading>
           </Cell>
 
         </Row>
 
 
 
-          
+
         <Row>
           <Cell>
-            <Text>Start Date  And End Date
-            </Text>
-          </Cell>
-          <Cell>
-            <Form>
-              <DatePicker name="s_date" label="Start Date" />
 
-              <DatePicker name="e_date" label="End Date" />
+            <Form onSubmit={onSubmit} /*actionButtons={actionButtons}*/>
+
+              <DatePicker name="s_date" label="Start Date" defaultValue={firstDay}  />
+
+              <DatePicker name="e_date" label="End Date" defaultValue={today}  />
             </Form>
 
           </Cell>
 
         </Row>
-        
 
-        
-          <Row>
-            <Cell>
-              <Text>Total Hours</Text>
-            </Cell>
-            
-            
-              <Cell>
-              <Text>{totalHours}</Text>
-            </Cell>
-            
-          </Row>
-        
+
+
+        <Row>
+
+          <Cell>
+
+            <Heading size='medium'>
+              
+             Total: {totalHours} Hours
+             
+            </Heading>
+
+          </Cell>
+
+        </Row>
+
       </Table>
 
       <Heading size="large"> Issue Overview  </Heading>
+
       <Table>
 
         <Head>
@@ -207,121 +206,155 @@ const App = () => {
         </Head>
 
 
+        {fetch_project.issues.map(issue => {
+          if ((moment(issue.fields.created).format('YYYY-MM-DD'))>=startDate  && (moment(issue.fields.created).format('YYYY-MM-DD')<=endDate)){
+          
 
-        <Row>
+        return <Row>
+
           <Cell>
-            {fetch_project.issues.map(issue => 
-            (            
+            
               <Text>{issue.key}</Text>
-            ))}
+            
           </Cell>
 
           <Cell>
-            {fetch_project.issues.map(issue => (
+            
+
               <Text>{issue.fields.summary}</Text>
-            ))}
+            
           </Cell>
 
           <Cell>
-            {fetch_project.issues.map(issue => (
+            
 
-              <Text>{(issue.fields.timespent)/3600}</Text>
-            ))}
+              <Text>{Math.round(issue.fields.timespent) / 3600}</Text>
+            
           </Cell>
         </Row>
 
+          }}
+        )}
 
 
 
       </Table>
+
+
+      <Heading size="large"> Work Log Details  </Heading>
+
+
+
+      {fetch_project.issues.map(issue => {
       
-      <Heading size="large"> Worklog Details  </Heading>
-      <Table>
+      if((moment(issue.fields.created).format('YYYY-MM-DD'))>=startDate  && (moment(issue.fields.created).format('YYYY-MM-DD')<=endDate)){
 
+     
+       return <Table>
 
-        <Head>
- 
-          <Cell>
-            
-            <Heading size="Medium">
-               
-              {fetch_project.issues.map(issue => 
-
-            (            
-              <Text>{issue.key}</Text>
-
-              
-            ))}
-
-            </Heading>
-
-            
-             
-
-            </Cell>
-
-          
-            <Cell>
-            <Heading size="Medium">{fetch_project.issues.map(issue => (
-              <Text>{issue.fields.summary}</Text>
-            ))}
-            </Heading>
-            </Cell>
-
+          <Head>
 
             <Cell>
-             
+
               <Heading size="Medium">
-            {fetch_project.issues.map(issue => (
 
-              <Text>{(issue.fields.timespent)/3600} Hours </Text>
-            ))}
-            </Heading>
+                <Text>{issue.key}</Text>
+
+              </Heading>
+
             </Cell>
+
+            <Cell>
+              <Heading size="Medium">
+
+                <Text>{issue.fields.summary}</Text>
+
+              </Heading>
+            </Cell>
+
+
+            <Cell>
+
+              <Heading size="Medium">
+
+                <Text>{Math.round(issue.fields.timespent) / 3600} Hours </Text>
+
+              </Heading>
+            </Cell>
+
+          </Head>
+
           
-        </Head>  
-              
-        <Row>
 
-        <Cell>
-        {all_WorkLogs.map(work=>(
-          <Fragment>
-            {work.map(worklog=>(
-              <Text>{worklog.started}</Text>
-            ))}
-          </Fragment>
-        ))}
-        </Cell>
-        
-        
-        <Cell>
-        {all_WorkLogs.map(work=>(
-          <Fragment>
-            {work.map(worklog=>(
-              <Text>{worklog.comment.content[0].content[0].text}</Text>
-            ))}
-          </Fragment>
-        ))}
-        </Cell>
+          {all_WorkLogs.map(work => {
+
+            
+            if (issue.id === work.issueId){
+             
+              return (<Row>
 
 
+                <Cell>
+                  <Text>{moment(work.started).format('DD MMM, YYYY,  HH:MM:SS')}</Text>
+                  
+                </Cell>
 
-        <Cell>
-        {all_WorkLogs.map(work=>(
-          <Fragment>
-            {work.map(worklog=>(
-              <Text>{(worklog.timeSpentSeconds)/3600}</Text>
-            ))}
-          </Fragment>
-        ))}
-        </Cell>
+                <Cell>{work.comment.content.map(multiComments=>{
+                   
+                     
+                      return <Fragment>
+                        
+                     {multiComments.content.map(bullets=>
+                      
+                      {
+                        if(bullets.content!=null){
+                          
+                           return <Fragment>
+                           <Text> - {bullets.content[0].content[0].text} </Text>
+                           {console.log(bullets.content[0].content[0].text)}
+                           </Fragment>
+                        }
+                        else{
+                          
+                            return <Fragment>
+                             <Text>{bullets.text} </Text>
+                            {console.log(bullets.text)}
+                            </Fragment>
 
-        </Row>
-        
-      
+                        }
+                        
+                      
+                      }
+                        
+                      )
+                      
+                    }</Fragment>
+                    
+                        
+                     }
+                     
+                      )
+                  }
 
-              
+                </Cell>
+
+                {/*console.log(all_WorkLogs[3].comment.content[0].content[1].content[0].content[0].text)*/}
+                <Cell>
+                  <Text>{Math.round(work.timeSpentSeconds) / 3600}</Text>
+                </Cell>
+
+
+              </Row>);
+            
+          }
+          })}
+
+
         </Table>
+      
+        }} 
+    
+  )}
 
     </Fragment >
   );
